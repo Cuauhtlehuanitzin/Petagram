cd5b1af0971f72425da5bfd268eed5a7e7bf2de7
Cuauhtlehuanitzin <jfsr78@gmail.com>
2016-05-25 12:53:18 -0500

    Inclusión de la imagen circular e inclusión de nuevos elementos en el recycler view

--- app/src/main/java/mx/unam/petagram/pojo/ActivityContact.java
+++ app/src/main/java/mx/unam/petagram/pojo/ActivityContact.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +26,11 @@ public class ActivityContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_contact);
+
+        Toolbar miActionBar= (Toolbar)findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     public void OnClick(View v) {
