8159e52369b4d68d329cb29f32f60efb54cf577c
Cuauhtlehuanitzin <jfsr78@gmail.com>
2016-05-25 11:36:35 -0500

    Inclusión de la imagen circular

--- app/src/main/java/mx/unam/petagram/pojo/ActivityAbout.java
+++ app/src/main/java/mx/unam/petagram/pojo/ActivityAbout.java
@@ -1,7 +1,11 @@
 package mx.unam.petagram.pojo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 import mx.unam.petagram.R;
 
@@ -11,5 +15,11 @@ public class ActivityAbout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_about);
+
+        Toolbar miActionBar= (Toolbar)findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
 }
